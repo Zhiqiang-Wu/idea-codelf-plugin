261d80b1fcfa5ed45dda37d9d644e224af82166d
wuzhiqiang <755960141@qq.com>
2023-11-21 18:03:58 +0800

    chore: icon

---
+++ src/main/java/wzq/codelf/plugin/CodelfIcons.java
@@ -0,0 +1,14 @@
+package wzq.codelf.plugin;
+
+import com.intellij.openapi.util.IconLoader;
+
+import javax.swing.Icon;
+
+/**
+ * @author 吴志强
+ * @date 2023/11/21
+ */
+public final class CodelfIcons {
+
+    public static final Icon SEARCH = IconLoader.getIcon("/search.svg", CodelfIcons.class);
+}
