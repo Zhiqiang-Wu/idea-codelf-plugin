04576701c96f5128530f502eca2d6c306201415c
wuzhiqiang <755960141@qq.com>
2023-11-23 14:07:44 +0800

    feat: Menu icon

---
+++ src/main/kotlin/wzq/codelf/plugin/CodelfIcons.kt
@@ -0,0 +1,14 @@
+package wzq.codelf.plugin
+
+import com.intellij.openapi.util.IconLoader
+import javax.swing.Icon
+
+/**
+ * @author 吴志强
+ * @date 2023/11/23
+ */
+object CodelfIcons {
+
+    @JvmField
+    val SEARCH: Icon = IconLoader.getIcon("/icons/search.svg", CodelfIcons::class.java)
+}
--- src/main/kotlin/wzq/codelf/plugin/action/SearchAction.kt
+++ src/main/kotlin/wzq/codelf/plugin/action/SearchAction.kt
@@ -4,12 +4,17 @@ import com.intellij.ide.browsers.BrowserLauncher
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
+import com.intellij.openapi.project.DumbAware
 
 /**
  * @author 吴志强
  * @date 2023/11/21
  */
-class SearchAction : AnAction() {
+class SearchAction : AnAction(), DumbAware {
+
+    init {
+        isEnabledInModalContext = true
+    }
 
     override fun actionPerformed(e: AnActionEvent) {
         val selectedText = e.getData(CommonDataKeys.EDITOR)?.selectionModel?.selectedText ?: ""
