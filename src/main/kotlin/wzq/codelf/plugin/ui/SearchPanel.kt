b911329f4e1e7b3f1f5897b6ae1a9a15f33910ae
wuzhiqiang <755960141@qq.com>
2023-11-24 23:04:06 +0800

    chore: ModifiedFlowLayout

--- src/main/kotlin/wzq/codelf/plugin/ui/SearchPanel.kt
+++ src/main/kotlin/wzq/codelf/plugin/ui/SearchPanel.kt
@@ -1,14 +1,16 @@
 package wzq.codelf.plugin.ui
 
 import com.intellij.ui.components.JBPanel
+import com.intellij.ui.components.JBScrollPane
 import java.awt.BorderLayout
 import javax.swing.BorderFactory
+import javax.swing.ScrollPaneConstants
 
 /**
  * @author 吴志强
  * @date 2023/11/24
  */
-class SearchPanel: JBPanel<SearchPanel>() {
+class SearchPanel : JBPanel<SearchPanel>() {
 
     init {
         this.layout = BorderLayout(0, 0)
@@ -16,6 +18,13 @@ class SearchPanel: JBPanel<SearchPanel>() {
 
         this.add(SearchNorthPanel(), BorderLayout.NORTH)
 
-        this.add(SearchCenterPanel(), BorderLayout.CENTER)
+        this.add(
+            JBScrollPane(
+                SearchCenterPanel(),
+                ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED,
+                ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER
+            ),
+            BorderLayout.CENTER
+        )
     }
 }
