e07022335bdb0b0a1c9f790d4ad40296cab165f1
wuzhiqiang <755960141@qq.com>
2023-11-25 12:39:29 +0800

    chore: Variable

--- src/main/kotlin/wzq/codelf/plugin/action/SearchAction.kt
+++ src/main/kotlin/wzq/codelf/plugin/action/SearchAction.kt
@@ -1,23 +1,18 @@
 package wzq.codelf.plugin.action
 
-import com.intellij.ide.browsers.BrowserLauncher
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
-import com.intellij.openapi.actionSystem.CommonDataKeys
-import com.intellij.openapi.project.DumbAware
+import com.intellij.openapi.wm.ToolWindowManager
 
 /**
  * @author 吴志强
- * @date 2023/11/21
+ * @date 2023/11/25
  */
-class SearchAction : AnAction(), DumbAware {
-
-    init {
-        isEnabledInModalContext = true
-    }
+class SearchAction : AnAction() {
 
     override fun actionPerformed(e: AnActionEvent) {
-        val selectedText = e.getData(CommonDataKeys.EDITOR)?.selectionModel?.selectedText ?: ""
-        BrowserLauncher.instance.browse("https://unbug.github.io/codelf/#${selectedText}")
+        val project = e.project ?: return
+        val toolWindow = ToolWindowManager.getInstance(project).getToolWindow("Codelf")
+        toolWindow?.show()
     }
 }
---
+++ src/main/kotlin/wzq/codelf/plugin/action/SearchInBrowserAction.kt
@@ -0,0 +1,23 @@
+package wzq.codelf.plugin.action
+
+import com.intellij.ide.browsers.BrowserLauncher
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.CommonDataKeys
+import com.intellij.openapi.project.DumbAware
+
+/**
+ * @author 吴志强
+ * @date 2023/11/21
+ */
+class SearchInBrowserAction : AnAction(), DumbAware {
+
+    init {
+        isEnabledInModalContext = true
+    }
+
+    override fun actionPerformed(e: AnActionEvent) {
+        val selectedText = e.getData(CommonDataKeys.EDITOR)?.selectionModel?.selectedText ?: ""
+        BrowserLauncher.instance.browse("https://unbug.github.io/codelf/#${selectedText}")
+    }
+}
